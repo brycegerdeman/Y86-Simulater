568548bc984c17347059169788dea2f1963ba5ec
brycegerdeman <gerdemanrb@appstate.edu>
2023-03-27 13:53:08 -0400

    working on decode

--- DecodeStage.C
+++ DecodeStage.C
@@ -7,6 +7,7 @@
 #include "D.h"
 #include "M.h"
 #include "W.h"
+#include "E.h"
 #include "Stage.h"
 #include "DecodeStage.h"
 #include "Status.h"
@@ -17,10 +18,15 @@
 bool DecodeStage::doClockLow(PipeReg ** pregs, Stage ** stages) {
 	D * dreg = (D *) pregs[DREG];
 	E * ereg = (E *) pregs[EREG];
-	uint64_t stat = dreg->getstat()->getOutput(), icode = dreg->geticode()->getOutput(), ifun = dreg->getifun()->getOutput(), valC = dreg->getvalC()-getOutput(), valA = 0, valB = 0;
+	uint64_t stat = dreg->getstat()->getOutput(),
+ 		icode = dreg->geticode()->getOutput(),
+		 ifun = dreg->getifun()->getOutput(),
+		 valC = dreg->getvalC()->getOutput(),
+		 valA = 0, valB = 0;
    	uint64_t dstE = RNONE, dstM = RNONE, srcA = RNONE, srcB = RNONE;
 
-	setEInput(ereg, stat, icode, ifun, valC, valA, valB, dstE, dstM, srcA, srcB)
+setEInput(ereg, stat, icode, ifun, valC, valA, valB, dstE, dstM, srcA, srcB);
+	return false;
 }
 
 
@@ -28,9 +34,9 @@ void DecodeStage::doClockHigh(PipeReg ** pregs) {
 	return;
 }
 
-void DecodeStage::setEInput(E * dreg, uint64_t stat, uint64_t icode, 
-                           uint64_t ifun, uint64_t valC, uint64_t valA,
-				   uint64_t valB, uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB) {
+void DecodeStage::setEInput(E * ereg, uint64_t stat, uint64_t icode, 
+	uint64_t ifun, uint64_t valC, uint64_t valA,
+	uint64_t valB, uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB) {
 	ereg->getstat()->setInput(stat);
    	ereg->geticode()->setInput(icode);
    	ereg->getifun()->setInput(ifun);
