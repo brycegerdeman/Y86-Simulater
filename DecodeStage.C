8bba33019f6b43e4e11fce69fc879e4cd0c3a13f
brycegerdeman <gerdemanrb@appstate.edu>
2023-03-27 13:14:27 -0400

    Still working on decode

--- DecodeStage.C
+++ DecodeStage.C
@@ -17,10 +17,10 @@
 bool DecodeStage::doClockLow(PipeReg ** pregs, Stage ** stages) {
 	D * dreg = (D *) pregs[DREG];
 	E * ereg = (E *) pregs[EREG];
-	uint64_t stat = dreg->getstat(), icode = dreg->geticode(), ifun = dreg->getifun(), valC = dreg->getvalC(), valA = 0, valB = 0;
+	uint64_t stat = dreg->getstat()->getOutput(), icode = dreg->geticode()->getOutput(), ifun = dreg->getifun()->getOutput(), valC = dreg->getvalC()-getOutput(), valA = 0, valB = 0;
    	uint64_t dstE = RNONE, dstM = RNONE, srcA = RNONE, srcB = RNONE;
 
-	setDInput
+	setEInput(ereg, stat, icode, ifun, valC, valA, valB, dstE, dstM, srcA, srcB)
 }
 
 
@@ -29,10 +29,16 @@ void DecodeStage::doClockHigh(PipeReg ** pregs) {
 }
 
 void DecodeStage::setEInput(E * dreg, uint64_t stat, uint64_t icode, 
-                           uint64_t ifun, uint64_t rA, uint64_t rB,
-				   uint64_t valC, uint64_t valP) {
+                           uint64_t ifun, uint64_t valC, uint64_t valA,
+				   uint64_t valB, uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB) {
 	ereg->getstat()->setInput(stat);
    	ereg->geticode()->setInput(icode);
    	ereg->getifun()->setInput(ifun);
-	ereg->
+	ereg->getvalC()->setvalC(valC);
+	ereg->getvalA()->setvalA(valA);
+	ereg->getvalB()->setvalB(valB);
+	ereg->getdstE()->setdstE(dstE);
+	ereg->getdstM()->setdstM(dstM);
+	ereg->getsrcA()->setsrcA(srcA);
+	ereg->getsrcB()->setsrcA(srcB);
 }
