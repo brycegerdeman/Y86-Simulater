f5ded2e1600455fac5420e5bee5a181ba95ec2ee
Will Riddick <riddickws@appstate.edu>
2023-03-27 12:34:01 -0400

    Implemented execute?

--- ExecuteStage.C
+++ ExecuteStage.C
@@ -22,6 +22,13 @@
  *         MemoryStage, WritebackStage instances)
  */
 bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages) {
+	E * ereg = (E *) pregs[EREG];	
+	M * mreg = (M *) pregs[MREG];	
+	uint64_t icode = 0, Cnd = 0, valE = 0, valA = 0, dstE = 0, dstM = 0;	
+	uint64_t stat = SAOK;
+	
+	
+	setMInput(mreg, stat, icode, Cnd, valE, valA, dstE, dstM);
 	return false;
 }
 
@@ -33,5 +40,25 @@ bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages) {
  * @param: pregs - array of the pipeline register (F, D, E, M, W instances)
  */
 void ExecuteStage::doClockHigh(PipeReg ** pregs) {
-	return;
+	M * mreg = (M *) pregs[MREG];	
+	
+	mreg->getstat()->normal();	
+	mreg->geticode()->normal();	
+	mreg->getCnd()->normal();
+	mreg->getvalE()->normal();
+	mreg->getvalA()->normal();
+	mreg->getdstE()->normal();
+	mreg->getdstM()->normal();
 }
+
+void ExecuteStage::setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t Cnd, 
+	uint64_t valE, uint64_t valA, uint64_t dstE, uint64_t dstM) {
+	mreg->getstat()->setInput(stat);	
+	mreg->geticode()->setInput(icode);	
+	mreg->getCnd()->setInput(Cnd);	
+	mreg->getvalE()->setInput(valE);	
+	mreg->getvalA()->setInput(valA);	
+	mreg->getdstE()->setInput(dstE);	
+	mreg->getdstM()->setInput(dstM);	
+}
+
